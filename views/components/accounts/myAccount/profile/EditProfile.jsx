3b9e9cd7ba4962f42bfbff888f8b8c775ce26bf9
Ename45 <enamesit@gmail.com>
2023-09-26 00:56:21 +0100

    edit profile completed

--- views/components/accounts/myAccount/profile/EditProfile.jsx
+++ views/components/accounts/myAccount/profile/EditProfile.jsx
@@ -11,14 +11,31 @@ import {
 import React, { useState } from "react";
 import { COLORS, SIZES } from "../../../../../constants/theme";
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
+import { Picker } from '@react-native-picker/picker';
 import CustomButton from "../../../reusable/button/CustomButton";
 import { imagesDataUrl, photos } from "../../../../../constants/data";
 import * as ImagePicker from "expo-image-picker";
+import InputField from "../../../reusable/inputField/InputField";
+import { useNavigation } from '@react-navigation/native';
 
-const EditProfile = ({ navigation }) => {
+const EditProfile = ({navigation}) => {
   const [selectedImage, setSelectedImage] = useState(imagesDataUrl[0]);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [houseNumber, setHouseNumber] = useState("");
+  const [street, setStreet] = useState("");
+  const [lga, setLga] = useState("");
+  const [state, setState] = useState("");
+  const [gender, setGender] = useState();
 
-  
+
+  // const navigation = useNavigation();
+
+  const handleUpdate = () => {
+    navigation.navigate("Settings")
+  }
 
   const handleImageSelection = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -34,62 +51,6 @@ const EditProfile = ({ navigation }) => {
     }
   };
 
-  const updateFormItems = [
-    {
-      name: "First Name",
-    },
-    {
-      name: "Last Name",
-    },
-    {
-      name: "PhoneNumber",
-    },
-    {
-      name: "Email",
-    },
-    {
-      name: "House Number",
-    },
-    {
-      name: "Street",
-    },
-    {
-      name: "LGA",
-    },
-    {
-      name: "State",
-    },
-    //  private Gender gender;
-  ];
-
-  const renderFormItem = ({ name }) => (
-    <View style={{ height: (12 / 100) * SIZES.height }}>
-      <View>
-        <Text
-          style={{
-            marginBottom: 5,
-            color: COLORS.gray3,
-            fontSize: SIZES.mid,
-            paddingLeft: 10,
-          }}
-        >
-          {name}
-        </Text>
-      </View >
-      <TextInput
-        style={{
-          borderWidth: 1,
-          borderColor: COLORS.gray3,
-          borderStyle: "solid",
-          borderRadius: 12,
-          height: "60%",
-          padding: 10,
-          fontSize: SIZES.medium,          
-        }}
-      />
-    </View>
-  );
-
   return (
     <SafeAreaView
       style={{
@@ -100,264 +61,271 @@ const EditProfile = ({ navigation }) => {
         padding: 20,
       }}
     >
-      <View>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            width: SIZES.width / 2,
-            justifyContent: "space-between",
-          }}
-        >
-          <View
-            style={{ backgroundColor: COLORS.tabBarBrown, borderRadius: 50 }}
-          >
-            <Pressable>
-              <MaterialIcons
-                name="keyboard-arrow-left"
-                size={24}
-                color={COLORS.white}
-              />
-            </Pressable>
-          </View>
-          <View>
-            <Text style={{ fontSize: SIZES.large, fontWeight: "bold" }}>
-              Edit Profile
-            </Text>
-          </View>
-        </View>
-
-        <ScrollView>
+      <View
+        style={{
+          width: "100%",
+          height: "100%",
+          padding: 5,
+        }}
+      >
+        <View>
           <View
             style={{
+              flexDirection: "row",
               alignItems: "center",
-              marginVertical: 22,
-              position: "relative",
+              width: SIZES.width / 2,
+              justifyContent: "space-between",
             }}
           >
-            <TouchableOpacity onPress={handleImageSelection}>
-              <Image
-                source={{ uri: selectedImage }}
-                style={{
-                  height: 170,
-                  width: 170,
-                  borderRadius: 85,
-                  borderWidth: 2,
-                  borderColor: COLORS.primary,
-                }}
-              />
-
-              <View
-                style={{
-                  position: "absolute",
-                  bottom: 3,
-                  right: 9,
-                  zIndex: 9999,
-                  borderRadius: 50,
-                  backgroundColor: COLORS.pageBackgroundBrown,
-                }}
-              >
+            <View
+              style={{ backgroundColor: COLORS.tabBarBrown, borderRadius: 50 }}
+            >
+              <Pressable>
                 <MaterialIcons
-                  name="photo-camera"
-                  size={32}
-                  color={COLORS.gray3}
+                  name="keyboard-arrow-left"
+                  size={24}
+                  color={COLORS.white}
+                />
+              </Pressable>
+            </View>
+            <View>
+              <Text style={{ fontSize: SIZES.large, fontWeight: "bold" }}>
+                Edit Profile
+              </Text>
+            </View>
+          </View>
+
+          <ScrollView style={{ padding: 15 }}>
+            <View
+              style={{
+                alignItems: "center",
+                marginVertical: 22,
+                position: "relative",
+              }}
+            >
+              <TouchableOpacity onPress={handleImageSelection}>
+                <Image
+                  source={{ uri: selectedImage }}
+                  style={{
+                    height: 170,
+                    width: 170,
+                    borderRadius: 85,
+                    borderWidth: 2,
+                    borderColor: COLORS.primary,
+                  }}
+                />
+
+                <View
+                  style={{
+                    position: "absolute",
+                    bottom: 3,
+                    right: 9,
+                    zIndex: 9999,
+                    borderRadius: 50,
+                    backgroundColor: COLORS.pageBackgroundBrown,
+                  }}
+                >
+                  <MaterialIcons
+                    name="photo-camera"
+                    size={32}
+                    color={COLORS.gray3}
+                  />
+                </View>
+              </TouchableOpacity>
+            </View>
+
+            <View>
+              <View style={{ height: (12 / 100) * SIZES.height }}>
+                <View>
+                  <Text
+                    style={{
+                      marginBottom: 5,
+                      color: COLORS.gray3,
+                      fontSize: SIZES.mid,
+                      paddingLeft: 10,
+                    }}
+                  >
+                    First Name
+                  </Text>
+                </View>
+                <InputField
+                  style={{
+                    width: "100%",
+                  }}
+                  value={firstName}
+                  onChangeText={(text) => setFirstName(text)}
                 />
               </View>
-            </TouchableOpacity>
-          </View>
+              <View style={{ height: (12 / 100) * SIZES.height }}>
+                <View>
+                  <Text
+                    style={{
+                      marginBottom: 5,
+                      color: COLORS.gray3,
+                      fontSize: SIZES.mid,
+                      paddingLeft: 10,
+                    }}
+                  >
+                    Last Name
+                  </Text>
+                </View>
+                <InputField
+                  style={{
+                    width: "100%",
+                  }}
+                  value={lastName}
+                  onChangeText={(text) => setLastName(text)}
+                />
+              </View>
+              <View style={{ height: (12 / 100) * SIZES.height }}>
+                <View>
+                  <Text
+                    style={{
+                      marginBottom: 5,
+                      color: COLORS.gray3,
+                      fontSize: SIZES.mid,
+                      paddingLeft: 10,
+                    }}
+                  >
+                    Phone Number
+                  </Text>
+                </View>
+                <InputField
+                  style={{
+                    width: "100%",
+                  }}
+                  value={phoneNumber}
+                  onChangeText={(text) => setPhoneNumber(text)}
+                />
+              </View>
+              <View style={{ height: (12 / 100) * SIZES.height }}>
+                <View>
+                  <Text
+                    style={{
+                      marginBottom: 5,
+                      color: COLORS.gray3,
+                      fontSize: SIZES.mid,
+                      paddingLeft: 10,
+                    }}
+                  >
+                    Email
+                  </Text>
+                </View>
+                <InputField
+                  style={{
+                    width: "100%",
+                  }}
+                  value={email}
+                  onChangeText={(text) => setEmail(text)}
+                />
+              </View>
+              <View style={{ height: (12 / 100) * SIZES.height }}>
+                <View>
+                  <Text
+                    style={{
+                      marginBottom: 5,
+                      color: COLORS.gray3,
+                      fontSize: SIZES.mid,
+                      paddingLeft: 10,
+                    }}
+                  >
+                    House Number
+                  </Text>
+                </View>
+                <InputField
+                  style={{
+                    width: "100%",
+                  }}
+                  value={houseNumber}
+                  onChangeText={(text) => setHouseNumber(text)}
+                />
+              </View>
+              <View style={{ height: (12 / 100) * SIZES.height }}>
+                <View>
+                  <Text
+                    style={{
+                      marginBottom: 5,
+                      color: COLORS.gray3,
+                      fontSize: SIZES.mid,
+                      paddingLeft: 10,
+                    }}
+                  >
+                    Street
+                  </Text>
+                </View>
+                <InputField
+                  style={{
+                    width: "100%",
+                  }}
+                  value={street}
+                  onChangeText={(text) => setStreet(text)}
+                />
+              </View>
+              <View style={{ height: (12 / 100) * SIZES.height }}>
+                <View>
+                  <Text
+                    style={{
+                      marginBottom: 5,
+                      color: COLORS.gray3,
+                      fontSize: SIZES.mid,
+                      paddingLeft: 10,
+                    }}
+                  >
+                    LGA
+                  </Text>
+                </View>
+                <InputField
+                  style={{
+                    width: "100%",
+                  }}
+                  value={lga}
+                  onChangeText={(text) => setLga(text)}
+                />
+              </View>
+              <View style={{ height: (12 / 100) * SIZES.height }}>
+                <View>
+                  <Text
+                    style={{
+                      marginBottom: 5,
+                      color: COLORS.gray3,
+                      fontSize: SIZES.mid,
+                      paddingLeft: 10,
+                    }}
+                  >
+                    State
+                  </Text>
+                </View>
+                <InputField
+                  style={{
+                    width: "100%",
+                  }}
+                  value={state}
+                  onChangeText={(text) => setState(text)}
+                />
+              </View>
+              <View style={{borderWidth: 1, borderColor: COLORS.gray, borderStyle: "solid", borderRadius: 10, marginBottom: (10/100*(SIZES.height)), marginTop: (2/100*(SIZES.height))}}>
+                <Picker 
+                  onValueChange={(text) => setGender(text)}
+                  selectedValue={gender}
+                >
+                  <Picker.Item  label="select gender" value="" />
+                  <Picker.Item label="Female" value="FEMALE" />
+                  <Picker.Item label="Male" value="MALE" />
+                  <Picker.Item label="Others" value="OTHERS" />
+                </Picker>
+              </View>
+            </View>
+            <CustomButton   
+            buttonName="Update Profile"
+            onPress={handleUpdate}
+            style={{ marginTop: -50}}
+            />
 
-          <View>
-            {
-              updateFormItems.map((item, index) => (
-                <React.Fragment key={index}>
-                  {renderFormItem(item)}
-                </React.Fragment>
-              ))
-            }
-          </View>
-        </ScrollView>
+          </ScrollView>
+        </View>
       </View>
     </SafeAreaView>
   );
 };
 
 export default EditProfile;
-
-//  private String firstName;
-//  private String lastName;
-//  private String phoneNumber;
-//  private String password;
-//  private Gender gender;
-//  private String email;
-//  private Role role;
-//  private Address address;
-//   private String houseNumber;
-// private String street;
-// private String lga;
-// private String state;
-
-{
-  /* <View
-          
-
-              
-            </View>
-          </TouchableOpacity>
-        </View>
-
-        <View>
-          <View
-            style={{
-              flexDirection: "column",
-              marginBottom: 6,
-            }}
-          >
-            <Text style={{ ...FONTS.h4 }}>Name</Text>
-            <View
-              style={{
-                height: 44,
-                width: "100%",
-                borderColor: COLORS.secondaryGray,
-                borderWidth: 1,
-                borderRadius: 4,
-                marginVertical: 6,
-                justifyContent: "center",
-                paddingLeft: 8,
-              }}
-            >
-              <TextInput
-                value={name}
-                onChangeText={(value) => setName(value)}
-                editable={true}
-              />
-            </View>
-          </View>
-
-          <View
-            style={{
-              flexDirection: "column",
-              marginBottom: 6,
-            }}
-          >
-            <Text style={{ ...FONTS.h4 }}>Email</Text>
-            <View
-              style={{
-                height: 44,
-                width: "100%",
-                borderColor: COLORS.secondaryGray,
-                borderWidth: 1,
-                borderRadius: 4,
-                marginVertical: 6,
-                justifyContent: "center",
-                paddingLeft: 8,
-              }}
-            >
-              <TextInput
-                value={email}
-                onChangeText={(value) => setEmail(value)}
-                editable={true}
-              />
-            </View>
-          </View>
-
-          <View
-            style={{
-              flexDirection: "column",
-              marginBottom: 6,
-            }}
-          >
-            <Text style={{ ...FONTS.h4 }}>Password</Text>
-            <View
-              style={{
-                height: 44,
-                width: "100%",
-                borderColor: COLORS.secondaryGray,
-                borderWidth: 1,
-                borderRadius: 4,
-                marginVertical: 6,
-                justifyContent: "center",
-                paddingLeft: 8,
-              }}
-            >
-              <TextInput
-                value={password}
-                onChangeText={(value) => setPassword(value)}
-                editable={true}
-                secureTextEntry
-              />
-            </View>
-          </View>
-
-          <View
-            style={{
-              flexDirection: "column",
-              marginBottom: 6,
-            }}
-          >
-            <Text style={{ ...FONTS.h4 }}>Date or Birth</Text>
-            <TouchableOpacity
-              onPress={handleOnPressStartDate}
-              style={{
-                height: 44,
-                width: "100%",
-                borderColor: COLORS.secondaryGray,
-                borderWidth: 1,
-                borderRadius: 4,
-                marginVertical: 6,
-                justifyContent: "center",
-                paddingLeft: 8,
-              }}
-            >
-              <Text>{selectedStartDate}</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-
-        <View
-          style={{
-            flexDirection: "column",
-            marginBottom: 6,
-          }}
-        >
-          <Text style={{ ...FONTS.h4 }}>Country</Text>
-          <View
-            style={{
-              height: 44,
-              width: "100%",
-              borderColor: COLORS.secondaryGray,
-              borderWidth: 1,
-              borderRadius: 4,
-              marginVertical: 6,
-              justifyContent: "center",
-              paddingLeft: 8,
-            }}
-          >
-            <TextInput
-              value={country}
-              onChangeText={(value) => setCountry(value)}
-              editable={true}
-            />
-          </View>
-        </View>
-
-        <TouchableOpacity
-          style={{
-            backgroundColor: COLORS.primary,
-            height: 44,
-            borderRadius: 6,
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Text
-            style={{
-              ...FONTS.body3,
-              color: COLORS.white,
-            }}
-          >
-            Save Change
-          </Text>
-        </TouchableOpacity>
-
-        {renderDatePicker()} */
-}
